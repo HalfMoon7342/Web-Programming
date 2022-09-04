29e4321912b20e014d677df93266ca2ec4f36d5b
HalfMoon7342 <sophia.baker333@gmail.com>
2022-09-04 15:05:48 -0500

    added pages
    New pages from class

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$('#btnLogin').on('click', function() {
+    let strEmail = $('#txtEmail').val();
+    let strPassword = $('#txtPassword').val();
+    console.log(strEmail);
+    console.log(strPassword);
+});
